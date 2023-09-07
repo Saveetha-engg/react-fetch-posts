4fcac18a93f3168b80bb85aedbcb275c2f68361a
sethu-ravichandran <rsethu1997@gmail.com>
2023-09-07 14:10:02 +0530

    Initial commit done, to save the file structure.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeaderComponent from './components/HeaderComponent/HeaderComponent'
+import PostComponent from './components/PostComponent/PostComponent'
+import FooterComponent from './components/FooterComponent/FooterComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <HeaderComponent/>
+      <PostComponent/>
+      <FooterComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
+
+
+
