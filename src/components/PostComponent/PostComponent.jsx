b6ce04858df4bf1b98e07b3cccf63539ce7ec2ba
sethu-ravichandran <rsethu1997@gmail.com>
2023-09-07 18:40:09 +0530

    Add. Made my fetch() function to get the consecutive posts upon button clicks.

--- src/components/PostComponent/PostComponent.jsx
+++ src/components/PostComponent/PostComponent.jsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 import './PostComponent.css'
 
 const PostComponent = () => {
+    const [counter, setCounter] = useState(1)
     const [postTitle, setPostTitle] = useState("")
     const [postData, setPostData] = useState("")
     const fetchData = async() => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${counter}`)
         const data =  await response.json()
+        setCounter (counter+1)
+        console.log(`The post id is ${counter}`);
         setPostTitle(data.title)
         setPostData(data.body)
+        
     }
   return (
     <React.Fragment>
